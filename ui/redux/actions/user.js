18008ac6e135fd49d1b9c44a03d61eb309c713ee
infinite-persistence <inf.persistence@gmail.com>
2022-02-04 11:38:41 +0800

    Populate has_verified_email for OIDC logins.
    The rest of the code uses this to indicate "is authenticated", but it will no longer be populated for the OIDC case. To reduce code changes everywhere, we'll just populate it here.

--- ui/redux/actions/user.js
+++ ui/redux/actions/user.js
@@ -134,7 +134,7 @@ export function doAuthenticate(
       Lbryio.getTokens().then((tokens) => {
         dispatch({
           type: ACTIONS.AUTHENTICATION_SUCCESS,
-          data: { user },
+          data: { user, accessToken: tokens.access_token },
         });
 
         if (shareUsageData) {
